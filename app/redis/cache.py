37e35e1af286a3ebaf155b6e3b8b64c4bcd84989
Chinmayanandu N K <nkchinmayanandunk@gmail.com>
2026-09-02 01:25:19 +0530

    added logging and fixed cache

--- app/redis/cache.py
+++ app/redis/cache.py
@@ -1,4 +1,4 @@
 from app.redis.redis_client import redis_client
 
-async def set_cache(short_code:str,short_url):
-    await redis_client.set(short_code,short_url)
+async def set_cache(short_code:str,url:str):
+    await redis_client.set(short_code,url=url)
--- app/services/url_service.py
+++ app/services/url_service.py
@@ -4,24 +4,36 @@ from app.db.shard import get_shard
 from sqlalchemy.exc import IntegrityError
 from app.schemas.shorten import ShortenResponse
 from app.redis.cache import set_cache
+import logging 
+
 async def shorten_url(url:str):
     attempt = 0
-    while attempt <= 5:
+    while attempt < 5:
         short_code = generate_unique_code()
         shard = get_shard(short_code=short_code)
         try:
             await add_shortend_url(shard=shard,short_code=short_code,url=url)
         except IntegrityError:
             attempt += 1
+            logging.warning("could not able to generate differnt short_code")
             continue
         break
     else:
+        logging.error(
+            "Failed to generate a unique short_code after %d attempts",
+            5,
+            exc_info=True
+        )
         raise RuntimeError("all attempts failed")
 
     try:
-        await set_cache(short_code=short_code,short_url=shorten_url)
-    except Exception as e:
-        pass
+        await set_cache(short_code=short_code,url=url)
+    except Exception:
+        logging.warning(
+            "Redis cache population failed for short_code=%s",
+            short_code,
+            exc_info=True,
+        )
 
     return ShortenResponse(
     short_code=short_code,
